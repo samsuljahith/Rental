9741e5828debe8bd3528154e67044cbe7be3c163
samsul jahith s <115388971+samsuljahith@users.noreply.github.com>
2025-03-03 06:49:47 +0530

    nothing changed

---
+++ src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const Landing
+ = () => {
+  return (
+    <div>
+        <div className="flex justify-center items-center h-[calc(100vh-64px)]">
+            <div className="text-4xl font-bold text-primary-500 bg-gray-600">Landing Page</div>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+"use clients";
+
+import StoreProvider from "@/state/redux";
+
+
+const Providers = ({children}:{children: React.ReactNode}) =>{
+    return(
+        <StoreProvider>
+            {children}
+        </StoreProvider>
+    )
+}
+export default Providers;
